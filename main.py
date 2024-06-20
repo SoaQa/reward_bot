4344bfbf293b4a515c8434fa1263abd926dd0408
soaqa <soaqa57@gmail.com>
2024-06-20 22:04:25 +0300

    simple winner

--- main.py
+++ main.py
@@ -1,47 +1,36 @@
 import json
 import random
-import sys
 
 
-def get_winner(filename='result.json', message_id=175):
+winners_cnt = 4
+message_id = 1101
+
+
+def get_winner(filename='result.json'):
+
+    assert message_id is not None
+
     with open(filename, "r", encoding='utf-8') as f:
         data = json.loads(f.read())
 
         unique_users = set()
         user_name_by_id = {}
 
-        for i in data["messages"]:
-            if i.get("reply_to_message_id") == message_id:
-                user_name_by_id[i["from_id"]] = f'{i["from_id"]}: {i["from"]}'
-                unique_users.add(i["from_id"])
+        for message in data["messages"]:
+            if message.get("reply_to_message_id") == message_id:
+                user_name_by_id[message["from_id"]] = message["from"]
+                unique_users.add(message["from_id"])
 
         users_cnt = len(unique_users)
 
-        while users_cnt >= 3:
-            unique_users = random.sample(list(unique_users), k=users_cnt)
+        print("Уникльных участников ", users_cnt)
 
-            print("Возможно им повезёт! \n")
-            for i in unique_users:
-                print(user_name_by_id[i])
-            print("")
+        input("Нажми Enter чтобы получить случайных 4 победителей!")
 
-            print(f"Текущее количество участников {users_cnt}!")
-            input("Нажми enter чтобы уменьшить его вдвое!")
-            users_cnt = int(users_cnt / 2)
-
-        print("")
-        print(f"Финал! Количество участников {users_cnt}")
-        unique_users = random.sample(unique_users, k=users_cnt)
-        for i in unique_users:
-            print(user_name_by_id[i])
-
-        print("")
-        input("Нажми Enter чтобы определить победителя! \n")
-        winner = random.sample(unique_users, k=1)[0]
-        print(f"Победил пользователь с ID и именем: {user_name_by_id[winner]}")
+        for i in random.sample(list(unique_users), k=winners_cnt):
+            print(i, " ", user_name_by_id[i])
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(f"message {sys.argv[1]}")
-    get_winner(message_id=int(sys.argv[1]))
+    get_winner()
